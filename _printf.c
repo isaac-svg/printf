0951fa89a4f724e12fd4d253376bb9f597d6ed6d
isaac-svg <isaacsakyi009@gmail.com>
2023-07-28 17:55:02 +0000

    _printf

--- _printf.c
+++ _printf.c
@@ -1,63 +1,69 @@
 #include "main.h"
 
+
+void print_to_buffer(char buffer[], int *buffer_index);
+
 /**
- * _printf - works same as  Printf function
- * @format: the  format string
- * Return: the  number of characters printed
+ * _printf - Custom printf function
+ * @format: Format string passed as arg
+ * Return: The nNumber of characters printed.
  */
 int _printf(const char *format, ...)
 {
-    int j, printed = 0, prnted_chars = 0;
-    int flags, width, precision, size, buff_index = 0;
-    va_list args;
-    char buffer[BUFF_SIZE];
+	int j, prnt_chars = 0;
+	int flgs, width, prcs, size, buffer_index = 0;
+	va_list args;
+	char buffer[BUFF_SIZE];
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (j = 0; format && format[j] != '\0'; j++)
-    {
-        if (format[j] != '%')
-        {
-            buffer[buff_index++] = format[j];
-            if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_index);
-            prnted_chars++;
-        }
-	else
-        {
-            print_buffer(buffer, &buff_index);
-            flags = get_flags(format, &j);
-            width = get_width(format, &j, list);
-            precision = get_precision(format, &j, list);
-            size = get_size(format, &i);
-            ++j;
-            printed = handle_print(format, &j, list, buffer, flags, width, precision, size);
-            if (printed == -1)
-                return (-1);
-            prnted_chars += printed;
-        }
-    }
+	for (j = 0; format && format[j] != '\0'; j++)
+	{
+		if (format[j] != '%')
+		{
+			buffer[buffer_index++] = format[j];
+			if (buffer_index == BUFF_SIZE)
+				print_to_buffer(buffer, &buffer_index);
+			prnt_chars++;
+		}
+		else
+		{
+			print_to_buffer(buffer, &buffer_index);
+			flgs = get_flags(format, &j);
+			width = get_width(format, &j, args);
+			prcs = get_precision(format, &j, args);
+			size = get_size(format, &j);
+			++j;
+			prnt_chars += handle_print(format, &j, args, buffer,
+				flgs, width, prcs, size);
+			if (prnt_chars == -1)
+			{
+				va_end(args);
+				return (-1);
+			}
+		}
+	}
 
-    print_buffer(buffer, &buff_index);
+	print_to_buffer(buffer, &buffer_index);
 
-    va_end(args);
+	va_end(args);
 
-    return (prnted_chars);
+	return (prnt_chars);
 }
 
 /**
- * print_buffer - Prints the contentents of the buffer
+ * print_to_buffer - Prints the contents of the buffer if it exists
  * @buffer: Array containing characters
- * @buff_ind: Index at which to add the next character
+ * @buffer_index: Index at which to add the next char
  */
-void print_buffer(char buffer[], int *buff_index)
+
+void print_to_buffer(char buffer[], int *buffer_index)
 {
-    if (*buff_index > 0)
-    {
-        write(1, &buffer[0], *buff_index);
-        *buff_index = 0;
-    }
+	if (*buffer_index > 0)
+		write(1, &buffer[0], *buffer_index);
+
+	*buffer_index = 0;
 }
